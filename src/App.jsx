69cfc82fa513497c7d409889b4db96b749641b62
Luis Marsiglia <marsiglia.business@gmail.com>
2022-11-22 17:29:10 -0400

    add ability to update products per row

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,54 @@ import axios from 'axios'
 function Layout({ children }) {
   return (
     <main className='min-h-screen w-full bg-gray-100 flex items-center justify-center px-4 py-16'>
-      <div className='max-w-4xl'>{children}</div>
+      <div className='w-full max-w-7xl'>{children}</div>
     </main>
   )
 }
 
-function Card({ product }) {
+function Card({ product, rows }) {
+  const descriptionClampByRows = {
+    1: 8,
+    2: 7,
+    3: 7,
+    4: 6,
+    5: 6,
+    6: 5,
+    7: 4,
+    8: 4,
+  }
+
+  const titleClampByRows = {
+    1: 3,
+    2: 2,
+    3: 2,
+    4: 2,
+    5: 2,
+    6: 1,
+    7: 1,
+    8: 1,
+  }
+
   return (
-    <div class='max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg max-h-[400px]'>
-      <img class='rounded-t-lg h-[50%] md:h-[30%] object-cover w-full' src={product.thumbnail} alt={`Picture of ${product.title}`} />
-      <div class='p-5'>
-        <h5 class='text-xl font-bold text-gray-900 line-clamp-1 md:line-clamp-2'>{product.title}</h5>
-        <h4 class='mb-3 text-lg font-medium text-gray-800 line-clamp-1'>{product.brand}</h4>
-        <p class='font-normal text-gray-700 line-clamp-3 md:line-clamp-6 pb-1'>{product.description}</p>
+    <div className='max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg max-h-[400px]'>
+      <img className='rounded-t-lg h-[50%] md:h-[40%] object-cover w-full' src={product.thumbnail} alt={`Picture of ${product.title}`} />
+      <div className='p-5'>
+        <h5 className={`text-xl font-bold text-gray-900 line-clamp-${titleClampByRows[rows]}`}>{product.title}</h5>
+        <h4 className='mb-3 text-lg font-medium text-gray-800 line-clamp-1'>{product.brand}</h4>
+        <p className={`font-normal text-gray-700 pb-1 line-clamp-${descriptionClampByRows[rows]}`}>{product.description}</p>
       </div>
     </div>
+  )
+}
+
+function Form({ onUpdateRows, rows }) {
+  return (
+    <form className='pb-4'>
+      <div className='flex flex-row gap-2 items-center'>
+        <label htmlFor='items-per-row' className='block mb-2 text-sm font-medium text-gray-900'>Products per row</label>
+        <input type='number' id='items-per-row' min={1} max={8} value={rows} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5' onChange={onUpdateRows} />
+      </div>
+    </form>
   )
 }
 
@@ -26,6 +59,12 @@ function App() {
   const [loading, setLoading] = useState()
   const [data, setData] = useState()
   const [errors, setErrors] = useState(false)
+  const [rows, setRows] = useState(4)
+
+  const onUpdateRows = (event) => {
+    event.preventDefault()
+    setRows(Number(event.target.value))
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -46,10 +85,6 @@ function App() {
     fetchData()
   }, [])
 
-  useEffect(() => {
-    console.log({ data })
-  }, [data])
-
   if (!data || loading) {
     return (
       <Layout>
@@ -68,10 +103,13 @@ function App() {
 
   return (
     <Layout>
-      <h1 class='text-4xl font-bold mb-8'>List of products</h1>
-      <div class='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4'>
+      <h1 className='text-4xl font-bold mb-8'>List of products</h1>
+
+      <Form onUpdateRows={onUpdateRows} rows={rows} />
+
+      <div className={`grid grid-cols-${rows} gap-4`}>
         {data.products.map((product) => {
-          return <Card key={product.id} product={product}></Card>
+          return <Card rows={rows} key={product.id} product={product}></Card>
         })}
       </div>
     </Layout>
