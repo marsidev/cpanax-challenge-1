022478ba5efdbfdfbb514a397fb51e34dbd354e1
Luis Marsiglia <marsiglia.business@gmail.com>
2022-11-23 02:52:21 -0400

    update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,5 +6,15 @@ module.exports = {
 	},
 	plugins: [
 		require('@tailwindcss/line-clamp')
+	],
+	safelist: [
+		'grid-cols-1',
+		'grid-cols-2',
+		'grid-cols-3',
+		'grid-cols-4',
+		'grid-cols-5',
+		'grid-cols-6',
+		'grid-cols-7',
+		'grid-cols-8'
 	]
 }
