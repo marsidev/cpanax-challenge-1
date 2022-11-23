9535efa01349b93c944d15f8a4231920f779b198
Luis Marsiglia <marsiglia.business@gmail.com>
2022-11-23 02:39:51 -0400

    scale card on hover

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ product }) {
 	return (
-		<article className={'max-w-sm w-full bg-white border border-gray-200 rounded-lg shadow-lg max-h-[400px]'}>
+		<article className={'hover:scale-105 transition-all max-w-sm w-full bg-white border border-gray-200 rounded-lg shadow-lg max-h-[400px]'}>
 			<img alt={`Picture of ${product.title}`} className={'h-[25vw] md:h-[15vw] lg:h-[10vw] xl:h-[8vw] w-full rounded-t-lg object-cover'} src={product.thumbnail} width='100%' />
 			<div className='px-2 md:px-4 py-2 md:py-4'>
 				<h5 className={'text-md md:text-xl font-bold text-gray-900 line-clamp-2 md:line-clamp-2'}>{product.title}</h5>
