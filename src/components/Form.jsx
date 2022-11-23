eeed4dff194606c1242a3388775909024933b013
Luis Marsiglia <marsiglia.business@gmail.com>
2022-11-23 02:47:33 -0400

    add range slider

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,8 +2,11 @@ export default function Form({ onUpdateRows, rows }) {
 	return (
 		<form className='pb-4' onSubmit={e => e.preventDefault()}>
 			<div className='flex flex-row gap-2 items-center'>
-				<label className='block mb-2 text-sm font-medium text-gray-900' htmlFor='items-per-row'>Products per row</label>
-				<input className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5' id='items-per-row' max={8} min={1} type='number' value={rows} onChange={onUpdateRows} />
+				<label className='block mb-2 text-md font-medium text-gray-900' htmlFor='items-per-row'>Products per row</label>
+
+				<input className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5' id='items-per-row' max={8} min={1} type='number' value={rows} onChange={onUpdateRows} />
+
+				<input className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer' id='items-per-row' max={8} min={1} step={1} type='range' value={rows} onChange={onUpdateRows} />
 			</div>
 		</form>
 	)
